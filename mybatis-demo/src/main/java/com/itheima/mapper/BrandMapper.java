71525f035925dee616925a7f1cccae53bb09e468
HGCM <1228688831@qq.com>
2023-02-08 23:54:15 +0800

    2-8

--- mybatis-demo/src/main/java/com/itheima/mapper/BrandMapper.java
+++ mybatis-demo/src/main/java/com/itheima/mapper/BrandMapper.java
@@ -1,9 +1,10 @@
 package com.itheima.mapper;
 
 import com.itheima.pojo.Brand;
-import com.itheima.pojo.User;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 public interface BrandMapper {
     /**
@@ -11,5 +12,24 @@ public interface BrandMapper {
      */
     public List<Brand> selectAll();
     public Brand selectById(int id);
+
+    /**
+     * 参数设置
+     *      1. 散装参数，如果方法中有多个参数 使用@Param("SQL占位符名称")
+     *      2. 对象参数
+     *          只需保证SQL中的参数名和实体类属性名对应上
+     *      3. map集合参数
+     *          只需保证SQL中的参数名和map集合的名称对应上
+     * @param status
+     * @param CompanyName
+     * @param brandName
+     * @return
+     */
+//    List<Brand> selectByCondition(@Param("status")int status,@Param("companyName")String CompanyName,@Param("brandName")String brandName);
+//    List<Brand> selectByCondition(Brand brand);
+    List<Brand> selectByCondition(Map map);
+
+    // 单条件动态查询
+    List<Brand> selectByConditionSingle(Brand brand);
 
 }
--- mybatis-demo/src/test/java/com/itheima/test/MybatisTest.java
+++ mybatis-demo/src/test/java/com/itheima/test/MybatisTest.java
@@ -2,6 +2,7 @@ package com.itheima.test;
 
 import com.itheima.mapper.BrandMapper;
 import com.itheima.pojo.Brand;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -10,7 +11,9 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MybatisTest {
     @Test
@@ -28,6 +31,7 @@ public class MybatisTest {
         System.out.println(brands);
         sqlSession.close();
     }
+
     @Test
     public void testSelectById() throws IOException {
         // 获取sqlsession工厂
@@ -44,4 +48,71 @@ public class MybatisTest {
         sqlSession.close();
     }
 
+    @Test
+    public void testSelectByCondition() throws IOException {
+        //接收参数
+
+        int status = 1;
+        String companyName = "华为";
+        String brandName = "华为";
+
+        // 处理接收参数
+        companyName = "%" + companyName + "%";
+        brandName = "%" + brandName + "%";
+
+        // 封装对象
+//        Brand brand = new Brand();
+//        brand.setStatus(status);
+//        brand.setCompanyName(companyName);
+//        brand.setBrandName(brandName);
+        Map map = new HashMap();
+        map.put("status", status);
+        map.put("companyName", companyName);
+//        map.put("brandName", brandName);
+        // 获取sqlsession工厂
+        // 1.加载mybatis的核心配置文件，获取SqlSessionFactory
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        BrandMapper brandMapper = sqlSession.getMapper(BrandMapper.class);
+//        List<Brand> brands = brandMapper.selectByCondition(status,companyName,brandName);
+        List<Brand> brands = brandMapper.selectByCondition(map);
+        System.out.println(brands);
+        sqlSession.close();
+    }
+    @Test
+    public void testSelectByConditionString() throws IOException {
+        //接收参数
+
+        int status = 1;
+        String companyName = "华为";
+        String brandName = "华为";
+
+        // 处理接收参数
+        companyName = "%" + companyName + "%";
+        brandName = "%" + brandName + "%";
+
+        // 封装对象
+        Brand brand = new Brand();
+        brand.setStatus(status);
+//        brand.setCompanyName(companyName);
+//        brand.setBrandName(brandName);
+
+        // 获取sqlsession工厂
+        // 1.加载mybatis的核心配置文件，获取SqlSessionFactory
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        BrandMapper brandMapper = sqlSession.getMapper(BrandMapper.class);
+//        List<Brand> brands = brandMapper.selectByCondition(status,companyName,brandName);
+        List<Brand> brands = brandMapper.selectByConditionSingle(brand);
+        System.out.println(brands);
+        sqlSession.close();
+    }
 }
